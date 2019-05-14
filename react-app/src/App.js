1d555a7f526fea73e8b3eb0df1e3d18279ea170a
xswatx <xswatx@naver.com>
2019-05-14 15:13:41 +0900

    완성 but create후 read에러 수정필요

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -4,13 +4,14 @@ import ReadContent from './components/ReadContent'
 import Toc from './components/Toc'
 import Control from './components/Control'
 import CreateContent from './components/CreateContent'
+import UpdateContent from './components/UpdateContent'
 
 class App extends Component {
   constructor(props){
     super(props);
     this.max_content_id=3;
     this.state ={
-      mode :'create',
+      mode :'welcome',
       selected_content_id: '2' ,
       Subject:{title:'WEB', sub:'world wide web!'},
       welcome:{title:'Welcome', desc:'Hello, React!!!'},
@@ -21,44 +22,81 @@ class App extends Component {
       ]
     }
   }
-  render() {
-    let _title, _desc, _article = null;
+  getReadContent(){
+    var i = 0;
+    while(i<this.state.Contents.length){
+      var data=this.state.Contents[i];
+      if(data.id === this.state.selected_content_id){
+        return data;
+      }
+      i++;
+  }
+  }
+  getContent(){
+    var _title, _desc, _article = null;
     if(this.state.mode ==='welcome'){
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
       _article = <ReadContent title={_title} desc={_desc}/>;
     }else if(this.state.mode==='read'){
-      let i = 0;
-      while(i<this.state.Contents.length){
-        if(this.state.Contents[i].id === this.state.selected_content_id){
-          _title = this.state.Contents[i].title;
-          _desc = this.state.Contents[i].desc;
-          break;
-        }
-        i++;
-    }
-    _article = <ReadContent title={_title} desc={_desc}/>;
+     var _Contents = this.getReadContent();
+    _article = <ReadContent title={_Contents.title} desc={_Contents.desc}/>;
     }else if(this.state.mode==='create'){
       _article = <CreateContent onSubmit={function(_title,_desc){
         this.max_content_id = this.max_content_id +1;
+        //concat은 원본을 쓰는것이 아니고 사본에다가 추가하는 함수
         var _contents = this.state.Contents.concat({
           id:this.max_content_id, title:_title, desc:_desc
         })
-        this.setState({Contents:_contents});
+        this.setState({Contents:_contents, mode:'read'});
+      }.bind(this)}/>
+    }else if(this.state.mode==='update'){
+      _Contents = this.getReadContent();
+      _article = <UpdateContent data={_Contents} onSubmit={function(_id, _title, _desc){
+        var _contents=Array.from(this.state.Contents);
+        var i = 0;
+        while(i < _contents.length){
+          if(_contents[i].id===_id){
+            _contents[i]={id:_id,title:_title,desc:_desc};
+            break;
+          }
+          i++;
+        }
+        this.setState({mode:'read', Contents:_contents})
       }.bind(this)}/>
     }
+    return _article;
+  }
+  render() {
+
     return (
       <div className="App">
         <Subject title={this.state.Subject.title} sub={this.state.Subject.sub} onChangePage={function(){
           this.setState({mode : "welcome"})
         }.bind(this)}/>
         <Control onChangeMode={function(_mode){
-          this.setState({mode:_mode})
+          if(_mode==='delete'){
+            if(window.confirm('Are you sure?')){
+              var _contents = Array.from(this.state.Contents);
+              var i = 0;
+              while(i < _contents.length){
+                if(_contents[i].id === this.state.selected_content_id){
+                  _contents.splice(i,1);
+                  break;
+                }
+                i++;
+              }
+              this.setState({mode:'welcome', Contents:_contents})
+            }
+            alert('deleted!!!')
+          }else{
+            this.setState({mode:_mode})
+          }
         }.bind(this)}/>
         <Toc data={this.state.Contents} onChangePage={function(id){
           this.setState({mode:"read", selected_content_id:id})
       }.bind(this)}/>
-        {_article}
+        {this.getContent()}
       </div>
     );
   }
---
+++ react-app/src/components/UpdateContent.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+
+
+
+class UpdateContent extends Component {
+  constructor(props){
+    super(props);
+    this.state ={
+      id:this.props.data.id,
+      title:this.props.data.title,
+      desc:this.props.data.desc
+    }
+  }
+  inputFormHandler(e){
+    this.setState({[e.target.name]:e.target.value})
+  }
+    render() {
+      return (
+        <article>
+          <h2>Update</h2>
+          <form action="/create_process" method="post" onSubmit={function(e){
+            e.preventDefault();
+            //e.target.title.value :제목에 입력한 값, e.target.desc.value : desc에 입력한 값
+            this.props.onSubmit(this.state.id, this.state.title, this.state.desc);
+          }.bind(this)}>
+          <input type="hidden" name="id" value={this.state.id}></input>
+            <p>
+            <input type="text" name="title" placeholder="title" value={this.state.title} onChange={this.inputFormHandler.bind(this)}></input>
+            </p>
+            <p>
+            <textarea name="desc" placeholder="desc" value={this.state.desc} onChange={this.inputFormHandler.bind(this)}></textarea>
+            </p>
+            <p>
+              <input type="submit"></input>
+            </p>
+          </form>
+          </article>
+      );
+    }
+  }
+
+
+
+  export default UpdateContent;
